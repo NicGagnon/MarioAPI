717908f70d64138d9a1f8e08d86a9af206372814
Nicolas Gagnon <Nicolas.z.gagnon@gmail.com>
2019-09-18 18:21:43 +0100

    BFS implemented and first correct answer is returned

--- app.py
+++ app.py
@@ -1,20 +1,51 @@
 # Author is Nicolas Gagnon 2019
-from graph import Graph
+from graph import Graph, Cell
+#import queue
 
 
 def find_shortest_path(N, grid):
   validate_grid(N, grid)
 
-  # Mark all the vertices as not visited
-  visited = [False] * N * N
   graph = Graph(N)
-
   for i, row in enumerate(grid):
     for j, value in enumerate(row):
       graph.insert(i, j, value)
 
+  mc = graph.get_mario()
   graph.draw()
 
+  parent = {}
+  queue = []
+  queue.append(mc)
+  while queue:
+    node = queue.pop(0)
+    if node.value == "p":
+      return backtrace(parent, mc, node)
+    for adjacent in graph.get_neighbors(node):
+      if node not in queue:
+        parent[adjacent] = node  # <<<<< record its parent
+        queue.append(adjacent)
+
+def backtrace(parent, start, end):
+    path = [end]
+    while path[-1] != start:
+      path.append(parent[path[-1]])
+    path.reverse()
+
+    word_path = []
+    for index in range(len(path)-1):
+      word_path.append(check_direction(path[index], path[index+1]))
+    return word_path
+
+def check_direction(cell_one, cell_two):
+  if cell_one.c < cell_two.c:
+    return "RIGHT"
+  elif cell_one.c > cell_two.c:
+    return "LEFT"
+  elif cell_one.r < cell_two.r:
+    return "DOWN"
+  else:
+    return "UP"
 
 def validate_grid(N, grid):
   if len(grid) <= 1 or N <= 1:
--- graph.py
+++ graph.py
@@ -2,9 +2,10 @@ class Graph:
 
   def __init__(self, n):
     self.graph = [[Cell(i, j, '-') for i in range(n)] for j in range(n)]
+    self.length = n
 
-  def insert(self, x, y, value):
-    self.graph[x][y] = Cell(x, y, value)
+  def insert(self, r, c, value):
+    self.graph[r][c] = Cell(r, c, value)
 
   def draw(self):
     for row in self.graph:
@@ -12,10 +13,33 @@ class Graph:
         print(cell.value, end="")
       print("")
 
+  def get_cell(self, r, c):
+    if 0 <= r <= self.length-1 and 0 <= c <= self.length-1:
+      print("Coordinates {}, {}".format(str(r), str(c)))
+      return self.graph[r][c]
+    else:
+      return ""
+
+  def get_neighbors(self, c):
+    candidates = [self.get_cell(c.r - 1, c.c), self.get_cell(c.r + 1, c.c),
+                  self.get_cell(c.r, c.c - 1), self.get_cell(c.r, c.c + 1)]
+    return [c for c in candidates if c is not ""]
+
+  def get_mario(self):
+    for row in self.graph:
+      for cell in row:
+        if cell.value == "m":
+          return cell
+
 
 class Cell:
-  def __init__(self, x, y, value):
-    self.x = x
-    self.y = y
+  def __init__(self, r, c, value):
+    self.r = r
+    self.c = c
     self.value = value
+
+  def set(self, r, c):
+    self.r = r
+    self.c = c
+
 
