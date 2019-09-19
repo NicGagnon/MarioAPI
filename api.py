51f517fb1685dddf0ec1bcabc7e45ac4e75f8926
Nicolas Gagnon <Nicolas.z.gagnon@gmail.com>
2019-09-19 17:25:46 +0100

    api accepts two parameters reflecting N and the grid and returns result

--- api.py
+++ api.py
@@ -1,12 +1,24 @@
-from flask import Flask
-from flask_restful import Api
+from flask import Flask, jsonify
+from flask_restful import Api, Resource, request
 from config import Config
 from database import db
+from app import find_shortest_path
+from werkzeug.routing import BaseConverter
+import json
 
+class StrListConverter(BaseConverter):
+  regex = r'.+(?:,\.+)*,?'
+
+  def to_python(self, value):
+    return [str(x) for x in value.split(',')]
+
+  def to_url(self, value):
+    return ','.join(str(x) for x in value)
 
 # Create an instance of flask
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.converters['str_list'] = StrListConverter
 
 # Initiate the database
 db.init_app(app)
@@ -24,8 +36,19 @@ def create_tables():
 def index():
     return "Hello, World!"
 
+@app.route('/mario/<int:n>/<str_list:grid>', methods=['GET'])
+def mario(n, grid):
+  try:
+    #n = request.args.get('n', None)
+    #grid = request.args.get('grid', None)
+    pp = find_shortest_path(n, grid)
+  except Exception:
+    return "Oops! That was not a valid grid.   Try again"
+  return jsonify(pp)
+
+
 # API endpoints
-#api.add_resource(find_shortest_path, '/people')
+# api.add_resource(find_shortest_path, '/mario/{}')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
