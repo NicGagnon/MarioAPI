790cca9ff8751a09f5be190da88bd9a055fdbbe4
Nicolas Gagnon <Nicolas.z.gagnon@gmail.com>
2019-09-20 11:51:06 +0100

    Added db model and logs any attempts to the database

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from database import db
 from app import find_shortest_path
 from config import StrListConverter
+from models.mario import MarioModel
 
 
 
@@ -13,6 +14,8 @@ app.url_map.converters['str_list'] = StrListConverter
 
 # Initiate the database
 db.init_app(app)
+
+
 
 
 @app.before_first_request
@@ -30,7 +33,16 @@ def index():
 def get(n, grid):
   try:
     pp = find_shortest_path(n, grid)
-  except Exception:
+    new_mario = MarioModel(n, grid, pp)
+    new_mario.save_to_db()
+    return jsonify(pp)
+  except Exception as e:
+    print(e)
     return "Oops! That was not a valid grid. Try again"
-  return jsonify(pp)
 
+
+"""# endpoint to get log detail by id
+@app.route("/log/<id>", methods=["GET"])
+def user_detail(id):
+    mario = MarioModel.query.get(id)
+    return user_schema.jsonify(mario)"""
---
+++ models/mario.py
@@ -0,0 +1,26 @@
+from database import db
+from datetime import datetime
+
+# SQLalchemy Model for each solution in the database
+class MarioModel(db.Model):
+  __tablename__ = 'mario'
+
+  index = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  time_log = db.Column(db.DateTime, default=datetime.utcnow)
+  size = db.Column(db.String(8))
+  grid = db.Column(db.PickleType)
+  solution = db.Column(db.PickleType)
+
+  def __init__(self, size, grid, solution):
+    self.time_log = datetime.now()
+    self.size = size
+    self.grid = grid
+    self.solution = solution
+
+  def save_to_db(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def delete_from_db(self):
+    db.session.delete(self)
+    db.session.commit()
